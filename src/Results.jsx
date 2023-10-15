a857f8bced3540488ea599c27a34838c1b6383d2
MehediHasan95 <mehedihasan5131@gmail.com>
2023-10-15 21:44:27 +0600

    done

--- src/Results.jsx
+++ src/Results.jsx
@@ -60,36 +60,6 @@ const results = ({ state }) => {
           <strong>:</strong> {state.wordSpacing} {state.wordSpacingUnit}
         </p>
       </div>
-
-      {/* <p>
-        <strong>Font Family:</strong> {state.fontFamily}
-      </p>
-      <p>
-        <strong>Size:</strong> {state.size} {state.sizeUnit}
-      </p>
-      <p>
-        <strong>Weight:</strong> {state.weight}
-      </p>
-      <p>
-        <strong>Transform:</strong> {state.transform}
-      </p>
-      <p>
-        <strong>Style:</strong> {state.style}
-      </p>
-      <p>
-        <strong>Decoration:</strong> {state.decoration}
-      </p>
-      <p>
-        <strong>Line-Height:</strong> {state.lineHeight} {state.lineHeightUnit}
-      </p>
-      <p>
-        <strong>Letter Spacing:</strong> {state.letterSpaceing}{" "}
-        {state.letterSpaceingUnit}
-      </p>
-      <p>
-        <strong>Word Spacing:</strong> {state.wordSpacing}{" "}
-        {state.wordSpacingUnit}
-      </p> */}
     </section>
   );
 };
